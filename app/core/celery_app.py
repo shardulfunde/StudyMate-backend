dbe248bc510acb74776f550c4499bb4be1ef59b9
Shardul Funde <fundeshardul@gmail.com>
2026-02-20 16:04:22 +0530

    Update celery_app.py

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -5,9 +5,6 @@ celery = Celery(
     broker="redis://localhost:6379/0",
 )
 
-celery.conf.task_routes = {
-    "app.tasks.embedding.generate_embeddings": {"queue": "embeddings"}
-}
 
 
 celery.conf.update(
